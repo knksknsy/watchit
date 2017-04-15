7f4a83f83027ef1ccbfa90533d8012cd39532868
Kaan <kk104@hdm-stuttgart.de>
2017-04-15 21:18:44 +0200

    Use input-group for search field.

--- WatchIt/src/app/app.component.ts
+++ WatchIt/src/app/app.component.ts
@@ -7,14 +7,11 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  hasQuery: boolean;
   query: string;
 
   constructor(private router: Router) { }
 
   searchQuery(query) {
-    this.hasQuery = query.length > 0;
-    this.hasQuery ? this.query = query : this.query = undefined;
     this.router.navigate(['/search', query]);
   }
 }
