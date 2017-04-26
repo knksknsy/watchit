044d380d64112753c552943f83d53984b1d851c5
Marci <marcellavoss@Marcellas-MacBook-Pro.local>
2017-04-26 17:22:35 +0200

    merge, add class btn-margin-right

--- WatchIt/src/app/components/popular-movie-card/popular-movie-card.component.ts
+++ WatchIt/src/app/components/popular-movie-card/popular-movie-card.component.ts
@@ -3,35 +3,46 @@ import { Router } from '@angular/router';
 import { IMovieResult } from '../../interfaces/movie-response';
 
 @Component({
-  selector: 'app-popular-movie-card',
-  templateUrl: './popular-movie-card.component.html',
-  styleUrls: ['./popular-movie-card.component.scss']
+	selector: 'app-popular-movie-card',
+	templateUrl: './popular-movie-card.component.html',
+	styleUrls: ['./popular-movie-card.component.scss']
 })
 export class PopularMovieCardComponent {
-  private _data: IMovieResult;
-  private _imgPath = 'https://image.tmdb.org/t/p/original';
+	private _data: IMovieResult;
+	private _imgPath = 'https://image.tmdb.org/t/p/original';
+	private _removable: boolean;
 
-  constructor(private router: Router) { }
+constructor(private router: Router) { }
 
-  get data(): IMovieResult {
-    return this._data;
-  }
+get data(): IMovieResult {
+	return this._data;
+}
 
-  @Input()
-  set data(data: IMovieResult) {
-    this._data = data;
-  }
+@Input()
+set data(data: IMovieResult) {
+	this._data = data;
+}
 
-  get imgPath(): string {
-    return this._imgPath;
-  }
+@Input()
+set removable(removable: boolean) {
+	this._removable = removable;
+}
 
-  set imgPath(imgPath: string) {
-    this._imgPath = imgPath;
-  }
+get removable(): boolean {
+	return this._removable;
+}
 
-  openDetails(id: number) {
-    this.router.navigate(['/details', id]);
-  }
+
+get imgPath(): string {
+	return this._imgPath;
+}
+
+set imgPath(imgPath: string) {
+	this._imgPath = imgPath;
+}
+
+openDetails(id: number) {
+	this.router.navigate(['/details', id]);
+}
 
 }
