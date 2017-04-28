d7a503ae12dae3f20ddaa68a66872231f3703b59
Marci <marcellavoss@Marcellas-MBP.fritz.box>
2017-04-28 20:26:05 +0200

    Responsiveness, Add Buttons (add to list), Add link to detail page

--- WatchIt/src/app/components/upcoming-movies/upcoming-movies.component.ts
+++ WatchIt/src/app/components/upcoming-movies/upcoming-movies.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { APIService } from '../../services/api/api.service';
 import { IMovieResponse } from '../../interfaces/movie-response';
 import { CarouselConfig } from 'ngx-bootstrap/carousel';
@@ -7,13 +8,13 @@ import { CarouselConfig } from 'ngx-bootstrap/carousel';
 	selector: 'app-upcoming-movies',
 	templateUrl: './upcoming-movies.component.html',
 	styleUrls: ['./upcoming-movies.component.scss'],
-	providers: [{provide: CarouselConfig, useValue: {interval: 3000000, noPause: true}}]
+	providers: [{provide: CarouselConfig, useValue: {interval: 7000, noPause: true}}]
 })
 export class UpcomingMoviesComponent implements OnInit {
 	public _response: IMovieResponse;
 	private _imgPath = 'https://image.tmdb.org/t/p/original';
 
-	constructor(private apiService: APIService) { }
+	constructor(private apiService: APIService, private router: Router) { }
 
 ngOnInit() {
 	this.apiService.getPopularMovies()
@@ -37,6 +38,10 @@ get imgPath(): string {
 
 set imgPath(imgPath: string) {
 	this._imgPath = imgPath;
+}
+
+openDetails(id: number) {
+	this.router.navigate(['/details', id]);
 }
 
 }
