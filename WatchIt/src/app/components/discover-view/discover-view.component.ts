be84a97ac14326c58273e12b376260fd18192ba8
Marci <marcellavoss@Marcellas-MacBook-Pro.local>
2017-05-21 23:28:17 +0200

    add discover-view component

---
+++ WatchIt/src/app/components/discover-view/discover-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-discover-view',
+  templateUrl: './discover-view.component.html',
+  styleUrls: ['./discover-view.component.scss']
+})
+export class DiscoverViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
