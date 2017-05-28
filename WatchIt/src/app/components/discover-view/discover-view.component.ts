d25160f2b7276b2c1ba73bf8c9738c1329438763
Marci <marcellavoss@Marcellas-MBP.fritz.box>
2017-05-29 00:28:50 +0200

    add content of accordion; add sorty_by dropdown; remove border-radius of .btn-secondary

--- WatchIt/src/app/components/discover-view/discover-view.component.ts
+++ WatchIt/src/app/components/discover-view/discover-view.component.ts
@@ -10,32 +10,24 @@ export class DiscoverViewComponent implements OnInit {
 	public oneAtATime: boolean = true;
 	public isFirstOpen: boolean = true;
 
-	//var selectedGenreArry : { [genre:string]:number; } = {};
 	selectedGenreArry = [];
+
+	yearTill: number;
+	yearFrom: number;
+	year: number;
+
+	public max:number = 10;
+	public rate:number = 7;
+
+	selectedValue: string = "release_date.asc";
+
 
 constructor() { }
 
 ngOnInit() {
 }
 
-/*updateSelectedGenreArray(id: number){
-	console.log("inside updateSelectedGenreArray -> " + this.selectedGenreArry.length);
-	var isFound: boolean = false;
 
-	for(var i = this.selectedGenreArry.length - 1; i >= 0; i--) {
-		if(this.selectedGenreArry[i] === id) {
-			this.selectedGenreArry.splice(i, 1);
-			isFound = true;
-			console.log("Id: " + id + " wurde entfernt.");
-		}
-	}
-	if(!isFound){
-		this.selectedGenreArry.push(id);
-		console.log("Id: " + id + " wurde hinzugefügt.");
-	}
-	
-	console.log("selectedGenreArry: " + this.selectedGenreArry);
-}*/
 updateSelectedGenreArray(id: number, cssId: string){
 	console.log("inside updateSelectedGenreArray -> " + this.selectedGenreArry.length);
 	var isFound: boolean = false;
@@ -54,7 +46,7 @@ updateSelectedGenreArray(id: number, cssId: string){
 		element.classList.add("selectedGenre");
 		console.log("Id: " + id + " wurde hinzugefügt.");
 	}
-	
+
 	console.log("selectedGenreArry: " + this.selectedGenreArry);
 }
 
